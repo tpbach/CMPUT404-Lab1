b7521e351abef69eb62804529ff8a10fd0aadb45
tpbach <tpbach@ualberta.ca>
2022-01-13 20:59:04 -0700

    Update lab1.py to pull github source code.

--- lab1.py
+++ lab1.py
@@ -1,7 +1,11 @@
+# CMPUT 404 - W22
+# Lab 1
+# Created by: tpbach
+
 import requests
 
 def main():
-    response = requests.get("https://www.google.com/")
-    print(response)
+    response = requests.get("https://raw.githubusercontent.com/tpbach/CMPUT404-Lab1/master/lab1.py")
+    print(response.text)
 
 main()
